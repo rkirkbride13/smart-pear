e293fa2d6583165fbc23d15895eb6f614d21799e
Robbie Kirkbride <robbie_kirkbride@hotmail.co.uk>
2023-05-30 09:41:02 +0100

    Test to create an authorizer added

--- spec/authorizer_spec.rb
+++ spec/authorizer_spec.rb
@@ -1,5 +1,7 @@
 require 'rspec'
 require 'google/apis/drive_v3'
+require 'googleauth'
+require 'googleauth/stores/file_token_store'
 require 'authorizer'
 
 RSpec.describe APIAuthorizer do
@@ -14,4 +16,16 @@ RSpec.describe APIAuthorizer do
       expect(@auth.sheet_id).to eq('12345')
     end
   end
+
+  describe '#create_authorizer' do
+    it 'creates authorizer with the correct credentials' do
+      expect(Google::Auth::ClientId).to receive(:from_file).with(@credentials_path).and_return('client_id')
+      expect(Google::Auth::Stores::FileTokenStore).to receive(:new).with(file: './config/token.json').and_return('token_store')
+      expect(Google::Auth::UserAuthorizer).to receive(:new).with('client_id', Google::Apis::DriveV3::AUTH_DRIVE, 'token_store').and_return('authorizer')
+
+      @auth.create_authorizer
+
+      expect(@auth.instance_variable_get(:@authorizer)).to eq('authorizer')
+    end
+  end  
 end
