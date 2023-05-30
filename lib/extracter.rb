752372d9bc99c73e92cbfe5c752af575eac961b1
Robbie Kirkbride <robbie_kirkbride@hotmail.co.uk>
2023-05-30 10:51:05 +0100

    Test of save_sheet_locallay passes and redundant initialize test removed

--- lib/extracter.rb
+++ lib/extracter.rb
@@ -1,6 +1,17 @@
+require 'google/apis/drive_v3'
+
 class Extracter
   def initialize(authorizeAPI)
     @credentials = authorizeAPI.get_API_credentials
     @sheet_id = authorizeAPI.sheet_id
+  end
+
+  def save_sheet_locally(sheet_name)
+    # Initialize Google Drive API client
+    drive_service = Google::Apis::DriveV3::DriveService.new
+    drive_service.authorization = @credentials
+    # Save the Google Sheet file
+    file = drive_service.get_file(@sheet_id, download_dest: "./files/#{sheet_name}")
+    puts "File saved as '#{sheet_name}'"
   end
 end
--- spec/extracter_spec.rb
+++ spec/extracter_spec.rb
@@ -2,30 +2,22 @@ require 'rspec'
 require 'extracter'
 
 describe Extracter do
-  
-  before(:each) do
-    @auth = double('APIAuthorizer')
-    @credentials = double('Google::Auth::UserAuthorizer')
-    @sheet_id = '12345'
-  end
-
-  describe '#initialize' do
-    it 'initializes with credentials and sheet ID' do
-      allow(@auth).to receive(:get_API_credentials).and_return(@credentials)
-      allow(@auth).to receive(:sheet_id).and_return(@sheet_id)
-    end
-  end
-
   describe '#save_sheet_locally' do
     it 'saves a spreadsheet locally' do
+      auth = double('APIAuthorizer')
+      credentials = double('Google::Auth::UserAuthorizer')
+      sheet_id = '12345'
       drive_service = double('Google::Apis::DriveV3::DriveService')
       sheet_name = 'mock_sheet'
+
+      allow(auth).to receive(:get_API_credentials).and_return(credentials)
+      allow(auth).to receive(:sheet_id).and_return(sheet_id)
       
-      extracter = Extracter.new(@auth)
+      extracter = Extracter.new(auth)
 
       allow(Google::Apis::DriveV3::DriveService).to receive(:new).and_return(drive_service)
-      expect(drive_service).to receive(:authorization=).with(@credentials)
-      expect(drive_service).to receive(:get_file).with(@sheet_id, download_dest: "./files/#{sheet_name}")
+      expect(drive_service).to receive(:authorization=).with(credentials)
+      expect(drive_service).to receive(:get_file).with(sheet_id, download_dest: "./files/#{sheet_name}")
       
       expect { extracter.save_sheet_locally(sheet_name) }.to output("File saved as '#{sheet_name}'\n").to_stdout
     end
