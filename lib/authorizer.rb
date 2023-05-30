937e1107aa0a0a1ef1d880559b7da5446c8c131f
Robbie Kirkbride <robbie_kirkbride@hotmail.co.uk>
2023-05-30 09:19:36 +0100

    APIAuthorizer created with initialize method

---
+++ lib/authorizer.rb
@@ -0,0 +1,14 @@
+require 'google/apis/drive_v3'
+
+class APIAuthorizer
+
+  def initialize(credentials_path, sheet_id)
+    # Path to the credentials JSON file obtained from the Google Cloud Console
+    @credentials_path = credentials_path
+    # ID of the Google Sheet file
+    @sheet_id = sheet_id
+    # Scope for the Google Drive API
+    @scope = Google::Apis::DriveV3::AUTH_DRIVE
+  end
+
+end
