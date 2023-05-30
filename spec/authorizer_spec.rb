52aa367937f8afd313e6cc3ae54c1c55921097de
Robbie Kirkbride <robbie_kirkbride@hotmail.co.uk>
2023-05-30 09:57:06 +0100

    Test to get API credentials  added

--- spec/authorizer_spec.rb
+++ spec/authorizer_spec.rb
@@ -27,5 +27,29 @@ RSpec.describe APIAuthorizer do
 
       expect(@auth.instance_variable_get(:@authorizer)).to eq('authorizer')
     end
-  end  
+  end
+  
+  describe '#get_API_credentials' do
+  
+    before(:each) do
+      client_id = instance_double('Google::Auth::ClientId')
+      token_store = instance_double('Google::Auth::Stores::FileTokenStore')
+      @authorizer = instance_double('Google::Auth::UserAuthorizer')
+      allow(Google::Auth::ClientId).to receive(:from_file).with(@credentials_path).and_return(client_id)
+      allow(Google::Auth::Stores::FileTokenStore).to receive(:new).with(file: './config/token.json').and_return(token_store)
+      allow(Google::Auth::UserAuthorizer).to receive(:new).with(client_id, @auth.instance_variable_get(:@scope), token_store).and_return(@authorizer)
+    end
+
+    
+    it 'prompts for authorization and returns new credentials' do
+      @auth.create_authorizer
+      allow(@authorizer).to receive(:get_credentials).and_return(nil)
+      allow(@authorizer).to receive(:get_authorization_url).and_return('http://authorization.url')
+      allow(@authorizer).to receive(:get_and_store_credentials_from_code).and_return(instance_double('Google::Auth::Credentials'))
+      allow_any_instance_of(Object).to receive(:gets).and_return('test_input')
+
+      expect { @auth.get_API_credentials }.to output(/Open the following URL in your browser and authorize the application/).to_stdout
+    end
+    
+  end
 end
