eac5b0b7bfc483db2ad6b674a01f7a403eeb82a4
Robbie Kirkbride <robbie_kirkbride@hotmail.co.uk>
2023-05-30 09:45:35 +0100

    Test of create authorizer method passing

--- lib/authorizer.rb
+++ lib/authorizer.rb
@@ -1,4 +1,6 @@
 require 'google/apis/drive_v3'
+require 'googleauth'
+require 'googleauth/stores/file_token_store'
 
 class APIAuthorizer
   attr_reader :sheet_id
@@ -10,6 +12,13 @@ class APIAuthorizer
     @sheet_id = sheet_id
     # Scope for the Google Drive API
     @scope = Google::Apis::DriveV3::AUTH_DRIVE
+  end
+
+  def create_authorizer
+    # Initialize the OAuth client ID and client secret
+    client_id = Google::Auth::ClientId.from_file(@credentials_path)
+    token_store = Google::Auth::Stores::FileTokenStore.new(file: './config/token.json')
+    @authorizer = Google::Auth::UserAuthorizer.new(client_id, @scope, token_store)
   end
 
 end
