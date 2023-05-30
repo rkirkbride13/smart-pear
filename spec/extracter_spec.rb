ce4eb564e5fe3999c99bbeaa63b12b6bc1b1ee89
Robbie Kirkbride <robbie_kirkbride@hotmail.co.uk>
2023-05-30 10:43:26 +0100

    Test of save_sheet_locally method

--- spec/extracter_spec.rb
+++ spec/extracter_spec.rb
@@ -15,4 +15,19 @@ describe Extracter do
       allow(@auth).to receive(:sheet_id).and_return(@sheet_id)
     end
   end
+
+  describe '#save_sheet_locally' do
+    it 'saves a spreadsheet locally' do
+      drive_service = double('Google::Apis::DriveV3::DriveService')
+      sheet_name = 'mock_sheet'
+      
+      extracter = Extracter.new(@auth)
+
+      allow(Google::Apis::DriveV3::DriveService).to receive(:new).and_return(drive_service)
+      expect(drive_service).to receive(:authorization=).with(@credentials)
+      expect(drive_service).to receive(:get_file).with(@sheet_id, download_dest: "./files/#{sheet_name}")
+      
+      expect { extracter.save_sheet_locally(sheet_name) }.to output("File saved as '#{sheet_name}'\n").to_stdout
+    end
+  end
 end
